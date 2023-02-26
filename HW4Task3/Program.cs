6f087e663b3e89ef6ff42f261ce0410a1d82576d
Anna Nikolenko <nikolenko_anmai@mail.ru>
2023-02-26 15:54:32 +0300

    task 3

---
+++ HW4Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] FillArray(int size)
+{
+    int [] userArray = new int [size];
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        userArray[i] = new Random().Next(0, 100);
+    }
+    return userArray;
+}
+
+void PrintArray(int[] array) //
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.WriteLine();
+}
+
+int InputSizeArray(string msg)
+{
+    System.Console.WriteLine($"{msg}");
+    return Convert.ToInt32 (Console.ReadLine());
+}
+int sizeArray = InputSizeArray("Введите размер массива");
+int[] userArray = FillArray(sizeArray);
+PrintArray(userArray);
